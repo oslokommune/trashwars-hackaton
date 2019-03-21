429132e61c0d2bf0a7d7c7ab9f801a752a371d9b
Jesper Kiledal <jesper.kiledal@gmail.com>
2019-03-21 19:46:34 +0100

    Add styling for leaderboard view

--- src/views/Leaderboard.js
+++ src/views/Leaderboard.js
@@ -1,29 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 const leaderboard = [
   {
     id: 1,
     name: 'Bøler søppelfotball',
-    score: 12855021,
+    score: 12855021
   },
   {
     id: 2,
     name: 'Hølaløkka løkkesøppel',
-    score: 812384821,
+    score: 812384821
   },
   {
     id: 3,
     name: 'Svartdalens svarte riddere',
-    score: 72483812,
-  },
-];
+    score: 72483812
+  }
+]
 
 export default class Leaderboard extends Component {
   render() {
     return (
-      <ul>
-        {leaderboard.map(l => (<li key={l.id}>{l.name}</li>))}
-      </ul>
-    );
+      <div className="leaderboard">
+        <ul className="leaderboard__nav">
+          <li className="leaderboard__navelement leaderboard__navelement--active">
+            Oslo
+          </li>
+          <li className="leaderboard__navelement">Grünerløkka</li>
+          <li className="leaderboard__navelement">Klan</li>
+        </ul>
+
+        <ol className="leaderboard__list">
+          {leaderboard.map(l => (
+            <li className="leaderboard__element" key={l.id}>
+              {l.name}
+            </li>
+          ))}
+        </ol>
+      </div>
+    )
   }
 }
