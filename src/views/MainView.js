254c47a757c47e029edab4f1ed0a8988d8bb962e
Jesper Kiledal <jesper.kiledal@gmail.com>
2019-03-21 20:15:27 +0100

    Add PWA meta tags

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,63 +1,63 @@
 // @flow
 
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import colors from '../style/colors';
-import { setRandomVariable, setCurrentView } from '../redux/actions/ui';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import colors from '../style/colors'
+import { setRandomVariable, setCurrentView } from '../redux/actions/ui'
 
 const mapStateToProps = state => {
   return {
     ui: state.ui
-  };
-};
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
     setRandomVariable: value => dispatch(setRandomVariable(value)),
     setCurrentView: currentView => dispatch(setCurrentView(currentView))
-  };
-};
+  }
+}
 
 type Props = {
   ui: Object,
   setRandomVariable: boolean => void,
   setCurrentView: string => void
-};
+}
 
-type State = {};
+type State = {}
 
 class MainView extends Component<Props, State> {
   renderActiveAreas() {
     return ['Birkelunden', 'Schous Plass'].map(area => {
       return (
-        <div className='list-item' key={area}>
+        <div className="list-item" key={area}>
           {area}
         </div>
-      );
-    });
+      )
+    })
   }
 
   render() {
-    const { ui, setCurrentView } = this.props;
+    const { ui, setCurrentView } = this.props
     return (
-      <div className='page'>
-        <div className='header'>
-          <div className='profile'>
-            <div className='profile__imagewrapper'>
-              <img src='/img/profile.png' className='profile__image' />
-              <a href='#' className='profile__imageupload' />
+      <div className="page">
+        <div className="header">
+          <div className="profile">
+            <div className="profile__imagewrapper">
+              <img src="/img/profile.png" className="profile__image" />
+              <a href="#" className="profile__imageupload" />
             </div>
           </div>
         </div>
-        <div className='pageContent'>
-          <div className='clan'>
-            <div className='title'>Løkka deTrashers</div>
+        <div className="pageContent">
+          <div className="clan">
+            <div className="title">Løkka deTrashers</div>
             <div>26 medlemmer</div>
           </div>
-          <div className='label'>Kontrolert område</div>
-          <div className='number'>13%</div>
-          <div className='label'>Poeng</div>
-          <div className='number'>6 728 600</div>
+          <div className="label">Kontrollert område</div>
+          <div className="number">13%</div>
+          <div className="label">Poeng</div>
+          <div className="number">6 728 600</div>
           <div>Aktive områder (2)</div>
           {this.renderActiveAreas()}
           <button onClick={() => setCurrentView('MAP')}>
@@ -65,8 +65,8 @@ class MainView extends Component<Props, State> {
           </button>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default (connect: any)(mapStateToProps, mapDispatchToProps)(MainView);
+export default (connect: any)(mapStateToProps, mapDispatchToProps)(MainView)
