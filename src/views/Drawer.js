8333bde33f3b0e86a3887f5e43e578ac9e80b47d
Håkon Gimse <hakongimse@gmail.com>
2019-03-22 12:12:30 +0100

    select clan in menu

--- src/views/Drawer.js
+++ src/views/Drawer.js
@@ -2,24 +2,35 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { setCurrentView, showDrawer } from '../redux/actions/ui';
+import {
+  setCurrentView,
+  showDrawer,
+  setSelectedClan
+} from '../redux/actions/ui';
+import { getRelevantClans } from '../selectors/clans';
 
 const mapStateToProps = state => {
   return {
-    ui: state.ui
+    ui: state.ui,
+    clans: state.clans,
+    user: state.user
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     setCurrentView: view => dispatch(setCurrentView(view)),
-    showDrawer: show => dispatch(showDrawer(show))
+    showDrawer: show => dispatch(showDrawer(show)),
+    setSelectedClan: clanId => dispatch(setSelectedClan(clanId))
   };
 };
 
 type Props = {
   ui: Object,
+  clans: Object,
+  user: Object,
   setCurrentView: Object => void,
+  setSelectedClan: (?string) => void,
   showDrawer: boolean => void
 };
 
@@ -40,6 +51,28 @@ class Drawer extends Component<Props, State> {
     this.props.showDrawer(!this.props.ui.showDrawer);
     this.props.setCurrentView('LEADERBOARD');
   };
+
+  renderClans() {
+    const { ui, clans, user, setSelectedClan } = this.props;
+    return getRelevantClans(clans, user.userId).map(clan => {
+      return (
+        <li
+          key={clan.clanId}
+          className={
+            ui.selectedClanId === clan.clanId
+              ? 'nav__link nav__link--active'
+              : 'nav__link'
+          }
+          onClick={() => {
+            setSelectedClan(clan.clanId);
+            this.onViewMain();
+          }}
+        >
+          {clan.clanName}
+        </li>
+      );
+    });
+  }
 
   render() {
     const { ui, showDrawer } = this.props;
@@ -70,16 +103,7 @@ class Drawer extends Component<Props, State> {
           </ul>
 
           <p className='label'>Min tilhørighet</p>
-          <ul className='nav__list'>
-            <li className='nav__link'>Cliff Kenneth Barterud</li>
-            <li
-              className='nav__link nav__link--active'
-              onClick={this.onViewMain}
-            >
-              Løkka deTrashers
-            </li>
-            <li className='nav__link'>Bøler Søppelfotball</li>
-          </ul>
+          <ul className='nav__list'>{this.renderClans()}</ul>
 
           <ul className='nav__list'>
             <li className='nav__link'>Innstillinger</li>
